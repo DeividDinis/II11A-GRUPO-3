27da72f5a130c06d905195552258e3d74981965a
Guilherme Bernardo <133515574+GuilhermeBernardo-byte@users.noreply.github.com>
2023-06-06 17:05:52 -0300

    Add files via upload

---
+++ App que envia mensagem para o Whatsapp/WS2.0.1/WS2.0/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WS2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsms = new System.Windows.Forms.TextBox();
+            this.txtdestino = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Location = new System.Drawing.Point(85, 285);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(75, 23);
+            this.btnenviar.TabIndex = 3;
+            this.btnenviar.Text = "Enviar";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Mensagem:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Destino(Nº):";
+            // 
+            // txtsms
+            // 
+            this.txtsms.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsms.Location = new System.Drawing.Point(85, 77);
+            this.txtsms.Multiline = true;
+            this.txtsms.Name = "txtsms";
+            this.txtsms.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtsms.Size = new System.Drawing.Size(339, 178);
+            this.txtsms.TabIndex = 2;
+            // 
+            // txtdestino
+            // 
+            this.txtdestino.Location = new System.Drawing.Point(85, 51);
+            this.txtdestino.Name = "txtdestino";
+            this.txtdestino.Size = new System.Drawing.Size(198, 20);
+            this.txtdestino.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(455, 327);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsms);
+            this.Controls.Add(this.txtdestino);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "";
+            this.Text = "What Send";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtsms;
+        private System.Windows.Forms.TextBox txtdestino;
+    }
+}
+
---
+++ App que envia mensagem para o Whatsapp/WS2.0.1/WS2.0/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WS2._0
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void envMW(string numero,string mensagem)
+        {
+            try
+            {
+                if (numero == "")
+                {
+                    MessageBox.Show("Digite o número de destino.");
+                    txtdestino.Focus();
+                }
+              else  if (mensagem == "")
+                {
+                    MessageBox.Show("Digite a mensagem que deseja enviar.");
+                    txtsms.Focus(); 
+                }
+                if (numero.Length==9)
+                {   
+                    numero = "+244" + numero;
+                    numero = numero.Replace(" ","");
+                System.Diagnostics.Process.Start("http://api.whatsapp.com/send?phone="+numero+"&text="+mensagem) ;
+                }
+
+              
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            envMW(txtdestino.Text,txtsms.Text);
+           
+        }
+    }
+}
